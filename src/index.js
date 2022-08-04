0c8591bf58b3121a152ce7c35e1b0e75a90cdaa9
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-04 19:49:22 +0300

    Add eventlisteners to enhance interactions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import updateScores from './modules/update';
+import clearFields from './modules/clearField';
+import fetchScores from './modules/fetchScores';
+
+document.querySelector('#submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const score = document.querySelector('#score').value;
+
+  if (name && score) {
+    updateScores(name, score);
+    clearFields();
+  }
+});
+
+document.querySelector('#refresh').addEventListener('click', () => {
+  document.location.reload();
+  fetchScores();
+});
+fetchScores();
